bf50eacb9150d0548d84bdce86b68b8dc3af4f32
Davi <lobpreal@gmail.com>
2023-12-06 19:24:37 -0300

    adicionado rota teste para deploy

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const { deploy } = require('../../controllers/deploy')
+const { deploy, db } = require('../../controllers/deploy')
 
 const rotas = express();
 
--- src/controllers/deploy.js
+++ src/controllers/deploy.js
@@ -1,5 +1,18 @@
+const knex = require('../config/connection/connection')
+
 const deploy = async (req, res) => {
-  return res.json({ mensagem: 'Deploy funcionando!' })
+  try {
+
+    const id = 1
+
+    const msg = await knex('deploy').select('mensagem').where({ id });
+
+    return res.json(msg);
+
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json('erro no servidor')
+  }
 }
 
 module.exports = {
