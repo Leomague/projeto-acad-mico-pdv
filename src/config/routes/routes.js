1be14fa85a5a764cdfc02440d3c0ff3fa77e4eb5
Thiago Tiburcio <thiagoactiburcio@gmail.com>
2023-12-15 21:45:09 -0300

    Inserção do endpoint CadastrarUsuario

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const cors = require('cors');
 const { deploy } = require('../../controllers/deploy');
 const { login } = require('../../controllers/login');
+const { cadastrarUsuario } = require('../../controllers/usuario')
 
 const rotas = express();
+
+rotas.post('/usuario', cadastrarUsuario);
 
 rotas.use(cors());
 rotas.use(express.json());
--- src/controllers/usuario.js
+++ src/controllers/usuario.js
@@ -1,4 +1,36 @@
-const cadastro = 'cadastrar usuario'
+const pool = require('../config/connection');
+const bcrypt = require('bcrypt');
+const { error400, error500 } = require('../config/chat/statusCode')
+
+
+const cadastrarUsuario = async (req, res) => {
+  const { nome, email, senha } = req.body;
+
+  try {
+    if (!nome || !email || !senha) {
+      return res.status(400).json({ error400, });
+    }
+    const emailExiste = await pool.query(
+      "select * from usuarios where email = $1",
+      [email]
+    );
+
+    if (emailExiste.rowCount > 0) {
+      return res.status(400).json({ error400 });
+    }
+
+    const senhaCriptografada = await bcrypt.hash(senha, 10);
+    const query =
+      "insert into usuarios (nome,email,senha) values ($1,$2,$3) returning *";
+
+    const { rows } = await pool.query(query, [nome, email, senhaCriptografada]);
+
+    const { senha: _, ...usuario } = rows[0];
+    return res.status(201).json(usuario);
+  } catch (error) {
+    return res.status(500).json({ error500 });
+  }
+};
 
 const login = 'login';
 
@@ -9,7 +41,7 @@ const detalharPerfi = 'detalhar perfil do usuario logado';
 const editarPerfil = 'editar perfil do usuario logado';
 
 module.exports = {
-  cadastro,
+  cadastrarUsuario,
   login,
   detalharPerfi,
   editarPerfil
