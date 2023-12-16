d5775f553c7f4e8064a44a603f7710c75944e1dc
SamuelDesenvolvedor <sleme600@gmail.com>
2023-12-16 18:26:41 -0300

    listar

--- src/config/routes/routes.js
+++ src/config/routes/routes.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const { deploy } = require('../../controllers/deploy')
+const { listarCategorias } = require('../../controllers/conta');
 
 const rotas = express();
 
 rotas.use(cors());
 rotas.use(express.json());
 
-rotas.get('/', deploy);
+rotas.get('/categoria', listarCategorias);
 
 module.exports = rotas;
--- src/controllers/conta.js
+++ src/controllers/conta.js
@@ -1,3 +1,20 @@
-const listarCategorias = 'listar categorias da tabela "cartegorias" do banco de dados';
-
-module.exports = listarCategorias;
+const listarCategorias = async (req, res) => {
+    try {
+      const query = 'SELECT nome FROM categorias';
+      const resultado = await pool.query(query);
+      const categorias = resultado.rows.map(row => row.nome);
+  
+      if (categorias.length > 0) {
+        return res.status(200).json(categorias);
+      } else {
+        return res.status(204).send();
+      }
+    } catch (error) {
+      return res.status(500).json({ mensagem: 'Erro interno no servidor' });
+    }
+  };
+  
+  module.exports = {
+    listarCategorias
+  };
+  
