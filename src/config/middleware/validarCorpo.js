17ccdd58ef72a4a96899ecda056a0361a210be05
Davi <lobpreal@gmail.com>
2023-12-18 19:38:53 -0300

    intermediario traduzido

--- src/config/middleware/validarCorpo.js
+++ src/config/middleware/validarCorpo.js
@@ -1,54 +1,38 @@
 const joi = require('joi')
 
-const schemaRegister = joi.object({
-  name: joi.string().min(3).max(30).required().messages({
-    'string.min': `The nome field must have at least ${3}, characters`,
-    'string.max': `The nome field must have at least ${30}, characters`,
-    'any.required': 'The nome field is mandatory',
-    'string.empty': 'The nome field is mandatory'
+const schemaCadastro = joi.object({
+  nome: joi.string().min(3).max(30).required().messages({
+    'string.min': `O campo nome deve ter pelo menos ${3} caracteres`,
+    'string.max': `O campo nome deve ter no máximo ${30} caracteres`,
+    'any.required': 'O campo nome é obrigatório',
+    'string.empty': 'O campo nome é obrigatório'
   }),
   email: joi.string().email().required().messages({
-    'string.email': 'The email field must have a valid format',
-    'any.required': 'The email field is mandatory',
-    'string.empty': 'The email field is mandatory'
+    'string.email': 'O campo de email deve ter um formato válido',
+    'any.required': 'O campo de email é obrigatório',
+    'string.empty': 'O campo de email é obrigatório'
   }),
-  password: joi.string().min(4).required().messages({
-    'string.min': `The password field must have at least ${4}, characters`,
-    'any.required': 'The password field is mandatory',
-    'string.empty': 'The password field is mandatory'
-  }),
-
-  age: joi.number().positive().messages({
-    'number.positive': 'Age must be a positive number',
-    'number.base': 'Age must be a number'
-  }),
-  active: joi.boolean().messages({
-    'boolean.base': 'the active field needs to be a boleean'
+  senha: joi.string().min(4).required().messages({
+    'string.min': `O campo de senha deve ter pelo menos ${4} caracteres`,
+    'any.required': 'O campo de senha é obrigatório',
+    'string.empty': 'O campo de senha é obrigatório'
   })
 })
 
 const schemaLogin = joi.object({
   email: joi.string().email().required().messages({
-    'string.email': 'The email field must have a valid format',
-    'any.required': 'The email field is mandatory',
-    'string.empty': 'The email field is mandatory'
+    'string.email': 'O campo de email deve ter um formato válido',
+    'any.required': 'O campo de email é obrigatório',
+    'string.empty': 'O campo de email é obrigatório'
   }),
-  password: joi.string().min(4).required().messages({
-    'string.min': `The password field must have at least ${4}, characters`,
-    'any.required': 'The password field is mandatory',
-    'string.empty': 'The password field is mandatory'
-  }),
-
-  age: joi.number().positive().messages({
-    'number.positive': 'Age must be a positive number',
-    'number.base': 'Age must be a number'
-  }),
-  active: joi.boolean().messages({
-    'boolean.base': 'the active field needs to be a boleean'
+  senha: joi.string().min(4).required().messages({
+    'string.min': `O campo de senha deve ter pelo menos ${4} caracteres`,
+    'any.required': 'O campo de senha é obrigatório',
+    'string.empty': 'O campo de senha é obrigatório'
   })
 })
 
 module.exports = {
-  schemaRegister,
+  schemaCadastro,
   schemaLogin
 }
